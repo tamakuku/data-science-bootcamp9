28e4e1d41e066081d9cffad0a029404b2de09f24
Suphakit Panthu (Top) <157778442+tamakuku@users.noreply.github.com>
2024-02-16 11:56:26 +0700

    Update Flights-Database.R

--- Portfolio-Project/R-Data-Transformation/Flights-Database.R
+++ Portfolio-Project/R-Data-Transformation/Flights-Database.R
@@ -1,41 +1,13 @@
+install.packgage("readr")
+install.packgage("sqldf")
+install.packgage("nycflights13")
+install.packgage("dplyr")
 
 library(readr)
 library(sqldf)
-library(nycflights13)
+library(nycflights13) # for use data set "flights"
 library(dplyr)
 
-## ****** Home Work 01 *******
-## install.packages("nycflights13")
-## call library(nycflights13)
-library(nycflights13)
-
-## 1. transform data name "flights"
-View(flights)
-
-## 2. use all function from data transformation live for make data by any question make up
-## use ?flights for see meaning each column that take for 5 questions
-
-## 3. send HW with Notion
-
-## 4. optional write answer by SQL syntax with library(sqldf)
-library(sqldf)
-
-## Answers Home Work 01 *****
-library(readr)
-write_csv(flights, "flights.csv")
-
-## Q&A
-## 1. How many avg flights delay on January?
-## 2. What month have longest distance flights?
-## 3. How many flights at noon time (6:00-18:00) on March?
-## 4. How many flights have first character of Plane tail number with N8?
-## 5. What is best five performance (shortest delay time) flight on 2013?
-
-## install.packages("nycflights13") and call library(nycflights13) for use dataframe name flights
-## install.packages("dpylr") and call library(dplyr) for use dpylr function
-install.packages( c( "nycflights13", "dplyr" ) )
-library(nycflights13)
-library(dplyr)
 
 ## use View(flight) for preview dataframe first.
 View(flights)
@@ -46,14 +18,14 @@ write_csv(flights, "flights.csv")
 
 flights_df <- flights
 
-## And list 5 question from flights dataframe
+### List 5 question from flights dataframe
 ## 1. What is the best five performance (shortest delay time) flight on 2013?
 ## 2. How many avg flights delay on January?
 ## 3. What month have longest distance flights?
 ## 4. How many flights at noon time (6:00-18:00) on March?
 ## 5. How many flights have first character of Plane tail number with N8?
-## then will use dplyr function with %>% pipeline operator for syntax style to find the answers.
 
+### use dplyr function with %>% pipeline operator for syntax style to find the answers.
 
 ## 1. What is the best five performance (shortest delay time) flight on 2013? *
 flights_df %>%
@@ -86,6 +58,7 @@ flights_df %>%
   group_by ( month ) %>%
   summarise ( count_flights = n() )
 ## answers: its have 20,440 flights at noon time on March.
+
 ## recheck preview result for sure
 flights_df %>%
   select ( month, dep_time ) %>%
@@ -99,6 +72,7 @@ flights_df %>%
   group_by ( carrier_airline ) %>%
   summarise ( count_fligths = n() )
 ## answers: its have 7,253 flights that tailnum N8... and come from UA carrier.
+
 ## recheck preview result for sure.
 flights_df %>%
   select ( tailnum, carrier ) %>%
